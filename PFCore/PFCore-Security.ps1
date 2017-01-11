29aee4216b7dd004a1d7f33faa99cb9f154ec581
gjones.pf@gmail.com <gjones.pf@gmail.com>
2017-01-12 10:31:19 +1300

    - Update params for SignChildren !deploy

--- PFCore/PFCore-Security.ps1
+++ PFCore/PFCore-Security.ps1
@@ -1,37 +1,47 @@
-function Invoke-SignChildren ($Directory, $Certificate, $Include)
+#($Directory, $Certificate, $Include)
+function Invoke-SignChildren 
 {
-   Push-Location $PWD
-   if(!$Directory)
-   {
-      $Directory = $PSScriptRoot
-   }
+    Param(
+       [Parameter(Position=0)]
+       $Directory,
+       [Parameter(Position=1)]
+       $Certificate = $(throw "The parameter -Certificate is required."),
+       [Parameter(Position=2)]
+       $Include
+    )
 
-   if(!$Include)
-   {
-      $Include = @("*.ps1","*.psm1","*.psd1","*.ps1xml")
-   }
+    Push-Location $PWD
+    if(!$Directory)
+    {
+        $Directory = $PSScriptRoot
+    }
 
-   $files = @(Get-ChildItem -Path "$Directory" -Include $Include -Recurse)
-   foreach($file in $files)
-   {
-      $filePath = Resolve-Path $file.DirectoryName
-      $fileIsInCurrentFolder = $filePath.ToString() -eq $PWD.ToString()
-      if(-not $fileIsInCurrentFolder)
-      {
-         Set-Location $filePath
-      }
-      $fileSig = (Get-AuthenticodeSignature -FilePath $file)
-      if($fileSig.Status -ne [System.Management.Automation.SignatureStatus]::Valid)
-      {
-         Write-Host -Foreground "Yellow" "File $file signature needs updating"
-         Set-AuthenticodeSignature $file $Certificate
-      }
-      else
-      {
-         Write-Host -Foreground "Green" "File $file has a valid signature"
-      }
-   }
+    if(!$Include)
+    {
+        $Include = @("*.ps1","*.psm1","*.psd1","*.ps1xml")
+    }
 
-   Pop-Location
+    $files = @(Get-ChildItem -Path "$Directory" -Include $Include -Recurse)
+    foreach($file in $files)
+    {
+        $filePath = Resolve-Path $file.DirectoryName
+        $fileIsInCurrentFolder = $filePath.ToString() -eq $PWD.ToString()
+        if(-not $fileIsInCurrentFolder)
+        {
+            Set-Location $filePath
+        }
+        $fileSig = (Get-AuthenticodeSignature -FilePath $file)
+        if($fileSig.Status -ne [System.Management.Automation.SignatureStatus]::Valid)
+        {
+            Write-Host -Foreground "Yellow" "File $file signature needs updating"
+            Set-AuthenticodeSignature $file $Certificate
+        }
+        else
+        {
+            Write-Host -Foreground "Green" "File $file has a valid signature"
+        }
+    }
+
+    Pop-Location
 }
 Export-ModuleMember -function Invoke-SignChildren
